ef089cb21a43c26fe4f3e1999de2548ec28d2cc0
Yuki TAKEI <nokonoko2057@gmail.com>
2020-05-05 17:21:30 +0900

    covid-19の名前をローカライズ

--- sketch.js
+++ sketch.js
@@ -6,7 +6,15 @@ var accessTokenSecret = '3bJ2iOl1nk9edbHsSUdayqy2AnWCBJKbyhkNZE4fPHWhY';
 
 const codeBird = new Codebird();
 
-var params;
+var covid19LocalizedName = [
+  "コロナウィルス",
+  "Coronavirus",
+  "코로나바이러스감염증",
+  "коронавирусной инфекции",
+  "Penyakit koronavirus",
+  "neumonía por coronavirus",
+  "कोरोना वायरस रोग"
+]
 
 var results = [];
 
@@ -16,24 +24,26 @@ function setup() {
   codeBird.setConsumerKey(consumerKey, consumerSecret);
   codeBird.setToken(accessToken, accessTokenSecret);
 
-  params = {
-    q: 'コロナウィルス',
-    result_type: 'recent',
-    count: 5
-  };
+  for (let i=0; i<covid19LocalizedName.length; i++) {
+    let params = {
+      q: covid19LocalizedName[i],
+      result_type: 'recent',
+      count: 10
+    };
+
+    codeBird.__call('search_tweets', params, (result) => {
+      for (let j=0; j<result.statuses.length; j++) {
+        print(result.statuses[j].text);
+        results.push(result.statuses[j].text);
+      }
+    });
+  }
 }
 
 function draw() {
   background(255);
-  textSize(32);
+  textSize(10);
   for (let i=0; i<results.length; i++) {
-    text(results[i], 100, i*50+100);
+    text(results[i], 100, i*30+100);
   }
-
-  codeBird.__call('search_tweets', params, (result) => {
-    for (let i=0; i<result.statuses.length; i++) {
-      print(result.statuses[i].text);
-      results[i] = result.statuses[i].text
-    }
-  });
 }
