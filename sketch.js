d77a66bf081cbef48a7ef5eea16cad9e8af292f7
Yuki TAKEI <nokonoko2057@gmail.com>
2020-05-05 16:10:20 +0900

    textで表示できるように修正

--- sketch.js
+++ sketch.js
@@ -18,7 +18,9 @@ const codeBird = new Codebird();
 
 var params;
 
-function setup() { 
+var results = [];
+
+function setup() {
   createCanvas(windowWidth, windowHeight);
 
   codeBird.setConsumerKey(consumerKey, consumerSecret);
@@ -29,19 +31,19 @@ function setup() {
     result_type: 'recent',
     count: 5
   };
-  
-  codeBird.__call('search_tweets', params, (result) => {
-    for (let i=0; i<result.statuses.length; i++) {
-      print(result.statuses[i].text);
-    }
-  });
 }
 
-function draw() { 
-  background(220);
+function draw() {
+  background(255);
+  text("aaaa", 100, 100);
+  for (let i=0; i<results.length; i++) {
+    text(results[i], 100, i*50+100);
+  }
+
   codeBird.__call('search_tweets', params, (result) => {
     for (let i=0; i<result.statuses.length; i++) {
       print(result.statuses[i].text);
+      results[i] = result.statuses[i].text
     }
   });
 }
