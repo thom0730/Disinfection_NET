3f868aa04dab943d461f467b1b66621bbfda034f
Yuki TAKEI <nokonoko2057@gmail.com>
2020-05-06 01:49:50 +0900

    該当する文字の時に黒塗りにする

--- sketch.js
+++ sketch.js
@@ -25,6 +25,7 @@ let filledKeywords = [
 var results = [];
 
 var combinedText = "";
+var charObjectOfText = [];
 let startCount;
 
 let margin = 300;
@@ -54,9 +55,12 @@ function setup() {
         let resultText = result.statuses[j].text
         resultText = resultText.replace(new RegExp('^RT ') ,'');
         resultText = resultText.replace(new RegExp('http.*') ,'');
+        addCharObjectOfText(resultText);
         results.push(result.statuses[j].text);
+
         combinedText += resultText;
       }
+      checkFilledKeyword();
     });
   }
 }
@@ -80,19 +84,9 @@ function draw() {
       let col = '#000000'; //パターン1
       //let col = '#C0B3A2'; //パターン2
       let ch = charText[i];
-      let rectWidth = 0.0;
-      for (let p=0; p<filledKeywords.length; p++) {
-        let keyword = filledKeywords[p];
-        for (let j=0; j < keyword.length; j++) {
-          let keywordChar = split(keyword,'');
-          if (keywordChar[j] != charText[i+j]) {
-            break;
-          }
-          rectWidth += textWidth(charText[i+j]);
-          if (keywordChar[keywordChar.length-1] == charText[i+j]) {
-            rect(drawPos.x, drawPos.y-tSize, rectWidth, tSize);
-          }
-        }
+      let charObject = charObjectOfText[i];
+      if (charObject.isFilled) {
+        rect(drawPos.x, drawPos.y-tSize, textWidth(charObject.char), tSize);
       }
 
   		//draw char
@@ -110,4 +104,53 @@ function draw() {
   		}
   	}
   };
+  print(charObjectOfText);
+}
+
+function addCharObjectOfText(sentence) {
+  let charTextArray = split(sentence,'');
+  for(let i = 0; i < charTextArray.length; i++) {
+    let charObject = new CharObject();
+    charObject.setChar(charTextArray[i]);
+    charObjectOfText.push(charObject);
+  }
+}
+
+function checkFilledKeyword() {
+  for(let charObjectIndex = 0; charObjectIndex < charObjectOfText.length; charObjectIndex++) {
+    for (let p = 0; p<filledKeywords.length; p++) {
+      let filledKeywordCharArray = split(filledKeywords[p],'');
+
+      //キーワードごとに該当する文字がキーワードと一致するか確認する
+      for (let keywordIndex = 0; keywordIndex < filledKeywordCharArray.length; keywordIndex++) {
+        if (charObjectOfText[charObjectIndex+keywordIndex].isFilled) { break; }
+
+        if (filledKeywordCharArray[keywordIndex] != charObjectOfText[charObjectIndex+keywordIndex].char) {
+          charObjectOfText[charObjectIndex+keywordIndex].setIsFilled(false);
+          break;
+        }
+
+        if (filledKeywordCharArray[filledKeywordCharArray.length-1] == charObjectOfText[charObjectIndex+keywordIndex].char) {
+          for (let z = 0; z <filledKeywordCharArray.length; z++) {
+            charObjectOfText[charObjectIndex+z].setIsFilled(true);
+          }
+        }
+      }
+    }
+  }
+}
+
+class CharObject {
+  setChar(char){
+    this.char = char;
+  }
+
+  setIsFilled(isFilled) {
+    this.isFilled = isFilled;
+  }
+
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
